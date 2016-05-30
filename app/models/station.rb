70bc1cd630ffd311a39328a503534aa958b7ced8
lovings2u <skyhan1106@naver.com>
2016-05-30 14:33:15 +0900

    Update station.rb

--- app/models/station.rb
+++ app/models/station.rb
@@ -3,7 +3,7 @@ require 'mailgun'
 class Station < ActiveRecord::Base
     belongs_to :univ
     def self.get_route_data (f_univ_name, t_univ_name)
-        uri = "http://ws.bus.go.kr/api/rest/pathinfo/getPathInfoByBus?ServiceKey=L4zwTXFwIMfC%2BITILlNDoDVLss3yxIDC%2Bcv8W6K5PrAYxRo7R%2BnDInlWMOW9o9JzwZhLXTK31ZC67ze8SCr2Xw%3D%3D"
+        uri = "http://ws.bus.go.kr/api/rest/pathinfo/getPathInfoByBus?ServiceKey=my service key"
         f_univ_data = Univ.where(name: f_univ_name).first.station
         t_univ_data = Univ.where(name: t_univ_name).first.station
         pre_count = 0;
@@ -26,7 +26,7 @@ class Station < ActiveRecord::Base
         mail_contents = "#{from_univ}에서 #{to_univ}로 가는 노선 정보가 매우 부족하거나 혹은.\n"+
                         "정보가 잘못되었습니다. 확인하시기 바랍니다."
 
-        mg_client = Mailgun::Client.new("key-3b85a1b74913346a0b3c407fae2c9986")
+        mg_client = Mailgun::Client.new("my api key")
         
         message_params =  {
                            from:    'admin@likelion.net',
@@ -35,7 +35,7 @@ class Station < ActiveRecord::Base
                            text:   mail_contents
                           }
         
-        mg_client.send_message('sandboxdbf2aecd8b5f4a72b7c66d017cbfa090.mailgun.org', message_params).to_h!
+        mg_client.send_message('my domain', message_params).to_h!
         result_message = "오류정보가 전달되었습니다.\n 빠른 시간 안에 해결하겠습니다.\n감사합니다."
     end
 end
